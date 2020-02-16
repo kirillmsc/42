a2f46e9c1ce63aa592b0933cc6511e53934f75ae
kirillmsc <kirillmsc@gmail.com>
2020-02-16 18:17:44 +0300

    calc

--- EvalExpr/func.c
+++ EvalExpr/func.c
@@ -1,52 +1,36 @@
-int		check_sign(char c)
-{
-	if (c == '(')
-		return (1);
-	else if ((c == '+') || (c == '-'))
-		return (2);
-	else if ((c == '*') || (c == '/'))
-		return (3);
-	return (0);
-}
-
-int		check_num(char n)
-{
-	if ((n >= '0') && (n <= '9'))
-		return (1);
-	return (0);
-}
+#include "func.h"
 
 void	ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(-nb);
-		return ;
-	}
-	if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-		ft_putchar(nb + '0');
+    if (nb < 0)
+    {
+        ft_putchar('-');
+        ft_putnbr(-nb);
+        return ;
+    }
+    if (nb >= 10)
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+    else
+        ft_putchar(nb + '0');
 }
 
 int		ft_atoi(char **str)
 {
-	int		nbr;
+    int		num;
 
-	nbr = 0;
-	while (**str >= '0' && **str <= '9')
-	{
-		nbr = nbr * 10 + (**str - '0');
-		(*str)++;
-	}
-	return (nbr);
+    num = 0;
+    while (**str >= '0' && **str <= '9')
+    {
+        num = num * 10 + (**str - '0');
+        (*str)++;
+    }
+    return (num);
 }
